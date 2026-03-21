2d1f04f337b8d2e50afb21fe9865f52d5fe337b0
xentriom <c04jason@gmail.com>
2026-03-20 23:08:02 -0400

    Create lang.js

---
+++ dictionary/lang.js
@@ -0,0 +1,18 @@
+/** @typedef {"de-de"|"en-us"|"es-mx"|"fr-fr"|"id-id"|"it-it"|"ja-jp"|"ko-kr"|"pt-br"|"ru-ru"|"th-th"|"vi-vn"|"zh-cn"|"zh-tw"} Language */
+
+export const lang = {
+  'de-de': 'Deutsch',
+  'en-us': 'English',
+  'es-mx': 'Español (México)',
+  'fr-fr': 'Français',
+  'id-id': 'Bahasa Indonesia',
+  'it-it': 'Italiano',
+  'ja-jp': '日本語',
+  'ko-kr': '한국어',
+  'pt-br': 'Português (Brasil)',
+  'ru-ru': 'Русский',
+  'th-th': 'ไทย',
+  'vi-vn': 'Tiếng Việt',
+  'zh-cn': '简体中文',
+  'zh-tw': '繁體中文',
+};
